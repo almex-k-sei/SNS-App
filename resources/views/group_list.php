f0ab1d5fbe5bcb8450d128a9ea3fd19e4370dd99
ishiiyukina <ykn-ishi@usen-next.jp>
2023-06-21 11:59:58 +0900

    6 viewfiles made

---
+++ resources/views/group_list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>グループリスト</title>
+    <!-- ホーム画面のフッターナビ「グループ」から飛ぶと閲覧できるグループの一覧
+　　グループをクリックすると、グループのメンバーが表示される-->
+</head>
+<body>
+    
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ホーム</title>
+    <!-- ログイン後一番最初に飛ぶホーム画面。画面設定3または4のイメージ-->
+</head>
+<body>
+    
+</body>
+</html>
