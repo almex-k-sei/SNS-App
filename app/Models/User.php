b6e07063374bfff6fa19da0ce05cef258b9027dc
kunimatsu9202 <k-kunimatsu@usen-next.jp>
2023-06-21 13:30:57 +0900

    UserとProfileのリレーション

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /* ユーザーは１つのプロフィールを所有 */
+    public function profile(){
+        return $this->hasOne(Profile::class);
+    }
+
+
 }
