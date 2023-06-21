b5530eb685df68399dabc558853bed0d4751b25d
kunimatsu9202 <k-kunimatsu@usen-next.jp>
2023-06-21 13:44:15 +0900

    Profileモデルにコメント追加

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    /* プロフィールは一人のユーザーに所属 */
     public function user(){
         return $this->belongsTo(User::class);
     }
