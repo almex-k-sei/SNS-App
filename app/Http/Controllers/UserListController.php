7f6c1529c66cf529239ef54255e607b67540ffa8
ishiiyukina <ykn-ishi@usen-next.jp>
2023-06-22 10:51:55 +0900

    homeに検索機能を付けた

--- app/Http/Controllers/UserListController.php
+++ app/Http/Controllers/UserListController.php
@@ -12,13 +12,19 @@ class UserListController extends Controller
 {
     //ホーム画面表示
     public function index(Request $request){
+        //ログイン情報から自分のプロフィールを取得→表示
         $user_id = Auth::id();
         $profiles = Profile::where('user_id','=',$user_id)
         ->get();
+        //フレンドを取得→表示
         $user = User::find($user_id);
         $friends = $user->follows;
-        // dd($friends);
-        $keyword = "あ";
+        //フレンド検索機能
+        $keyword = $request->input('keyword');
+        if($keyword !== null ){
+                $friends = $user->follows()->where('name','LIKE',"%$keyword%")->get();
+                // ->orWhere('birth','LIKE',"%$keyword%")->orwhere('description','LIKE',"%$keyword%")->get();
+        }
 
         return view('/home',compact('keyword','profiles','friends'));
 
