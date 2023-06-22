d96f1b69025949afd356e68401732610e71d751e
kijo <kijo@mail>
2023-06-22 14:54:29 +0900

    マイグレーションファイル作成

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -25,6 +25,7 @@
         <input type="text" name="content">
         <input type="hidden" name="user_id" value={{$my_id}}>
         <input type="hidden" name="talkroom_id" value={{$talkroom->id}}><!-- 現在会話しているトークルームのIDを保持 --->
+        <input type="file" id="avatar" name="avatar">
         <input type="submit" value="送る">
         @csrf
     </form>
