4a470f2a750645c4beddc845079904bbc9eb9b45
honda0429 <nh-honda@usen-next.jp>
2023-06-23 16:05:34 +0900

    edit_css8

--- resources/views/message_list.blade.php
+++ resources/views/message_list.blade.php
@@ -14,34 +14,34 @@
             </form>
         </div>
 
-        <div class="list_contents_container">
-            <div>
-            {{-- トークルーム名とそのトークルームの最後のメッセージを表示 --}}
-            @foreach ($talkrooms as $talkroom)
-                <form action="/Message" method="POST">
-                    <button>
-                        @if ($talkroom->name == "")
-                            <h2>
-                                @foreach ($talkroom->user as $user)
-                                    @if ($user->id != $user_id)
-                                        {{$user->name}}　
-                                    @endif
-                                @endforeach
-                            </h2>
-                        @else
-                            <h2>{{$talkroom->name}}</h2>
-                        @endif
-                        @if ($talkroom->message->last() !== NULL)
-                            <p>最後のメッセージ：{{$talkroom->message->last()->content}}</p>
-                        @endif
-                        <input type="hidden" name="id" value={{$talkroom->id}}>
-                    </button>
-                    @csrf
-                </form>
-            @endforeach
+            <div class="list_contents_container">
+                <div>
+                {{-- トークルーム名とそのトークルームの最後のメッセージを表示 --}}
+                @foreach ($talkrooms as $talkroom)
+                    <form action="/Message" method="POST">
+                        <button>
+                            @if ($talkroom->name == "")
+                                <h2>
+                                    @foreach ($talkroom->user as $user)
+                                        @if ($user->id != $user_id)
+                                            {{$user->name}}　
+                                        @endif
+                                    @endforeach
+                                </h2>
+                            @else
+                                <h2>{{$talkroom->name}}</h2>
+                            @endif
+                            @if ($talkroom->message->last() !== NULL)
+                                <p>最後のメッセージ：{{$talkroom->message->last()->content}}</p>
+                            @endif
+                            <input type="hidden" name="id" value={{$talkroom->id}}>
+                        </button>
+                        @csrf
+                    </form>
+                @endforeach
+                </div>
             </div>
-        </div>
 
-    </div>
+        </div>
 
 @include('footer')
