b2284c5870a668cf729b5a9f408f876d0d951a24
kijo <kijo@mail>
2023-06-23 16:45:55 +0900

    モーダルウィンドウ追加

--- resources/views/group_list.blade.php
+++ resources/views/group_list.blade.php
@@ -11,17 +11,37 @@
             </label>
             <input type="submit" value="検索">
         </form>
-    </div>
+        <!-- モーダルウィンドウのトリガーボタン -->
+        <button class="modalBtn">追加</button>
+
+        <!-- モーダルウィンドウのコンテンツ -->
+        <div class="modal">
+            <div class="modal-content">
+                <!-- コンテンツをここに追加します -->
+                <span class="close">&times;</span>
+            </div>
+        </div>
+
 
 
     <div class="list_contents_container">
         <div>
             {{-- トークルーム名とそのトークルームの最後のメッセージを表示 --}}
             @foreach ($groups as $group)
-                <button>
+                <button class="modalBtn">
                     <img src="{{ $group->image }}" height="100" width="100">
                     <h2>{{ $group->name }}</h2>
                 </button>
+                <!-- モーダルウィンドウのコンテンツ -->
+                <div class="modal">
+                    <div class="modal-content">
+                        <!-- コンテンツをここに追加します -->
+                        @foreach ($group->user as $user)
+                            <p>{{$user->name}}</p>
+                        @endforeach
+                        <span class="close">&times;</span>
+                    </div>
+                </div>
             @endforeach
         </div>
     </div>
@@ -30,3 +50,79 @@
 
 @include('footer')
 
+<style>
+/* モーダルウィンドウ */
+.modal {
+    display: none; /* 最初は非表示にする */
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5); /* 背景を半透明にする */
+}
+
+/* モーダルウィンドウのコンテンツ */
+.modal-content {
+    background-color: #fefefe;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 80%;
+}
+
+/* 閉じるボタン */
+.close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+</style>
+
+<script>
+'use strict';
+// モーダルウィンドウのトリガーボタンを取得
+var modalBtns = document.getElementsByClassName("modalBtn");
+
+// モーダルウィンドウの要素を取得
+var modals = document.getElementsByClassName("modal");
+
+// 閉じるボタンを取得
+var closeBtns = document.getElementsByClassName("close");
+
+// トリガーボタンがクリックされた時の処理
+for (var i = 0; i < modalBtns.length; i++) {
+    modalBtns[i].onclick = function() {
+        var modal = this.nextElementSibling;
+        modal.style.display = "block"; // 対応するモーダルを表示する
+    };
+}
+
+// 閉じるボタンがクリックされた時の処理
+for (var i = 0; i < closeBtns.length; i++) {
+    closeBtns[i].onclick = function() {
+        var modal = this.parentNode.parentNode;
+        modal.style.display = "none"; // 対応するモーダルを非表示にする
+    };
+}
+
+// モーダルウィンドウの外側がクリックされた時の処理
+window.onclick = function(event) {
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none"; // 対応するモーダルを非表示にする
+        }
+    }
+};
+
+</script>
