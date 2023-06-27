8c96d5713515a81879524892b6fc871a1a4fb37e
kijo <kijo@mail>
2023-06-27 17:00:10 +0900

    モーダルウィンドウ修正

--- resources/views/group_list.blade.php
+++ resources/views/group_list.blade.php
@@ -32,11 +32,8 @@
                             <select name="members[]" multiple>
                                 @foreach ($all_friends as $friend)
                                     <option value="{{ $friend->id }}"
-                                        @if(Auth::id() == $friend->id )
-                                            selected
-                                        @endif
-                                        >
-                                        {{$friend->profile->name}}
+                                        @if (Auth::id() == $friend->id) selected @endif>
+                                        {{ $friend->profile->name }}
                                     </option>
                                 @endforeach
                             </select>
@@ -60,7 +57,7 @@
                 {{-- グループ一覧の表示 --}}
                 @foreach ($groups as $group)
                     <button class="modalBtn">
-                        <img src="{{$group->image}}" width="200px" height="200px">
+                        <img src="{{ $group->image }}" width="200px" height="200px">
                         <h2>{{ $group->name }}</h2>
                     </button>
                     <!-- モーダルウィンドウのコンテンツ -->
@@ -69,44 +66,53 @@
                             <!-- コンテンツをここに追加します -->
 
                             <form action="/GroupList/quit" method="POST">
-                                <input type="hidden" name="user_id" value="{{Auth::id()}}">
-                                <input type="hidden" name="group_id" value="{{$group->id}}">
+                                <input type="hidden" name="user_id" value="{{ Auth::id() }}">
+                                <input type="hidden" name="group_id" value="{{ $group->id }}">
                                 <input type="submit" value="退会">
                                 @csrf
                             </form>
                             <h2>メンバー</h2>
                             {{-- メンバーを一覧表示 --}}
-                            @foreach ($group->user as $member)
-                                <p>{{ $member->name }}</p>
-                            @endforeach
+                            <div id="add_before_members">
+                                @foreach ($group->user as $member)
+                                    <p>{{ $member->name }}</p>
+                                @endforeach
+                            </div>
+                            <div id='add_after' class="hidden">
+                                <select name="" multiple>
+                                    @foreach ($group->user as $member)
+                                        <option value="{{ $member->id }}"
+                                            @if (Auth::id() == $member->id) selected @endif>
+                                            {{ $member->name }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
                             {{-- トークへ移動ボタン --}}
                             <form action="/Message" method="POST">
-                                <input type="hidden" name="id" value="{{$group->id}}">
+                                <input type="hidden" name="id" value="{{ $group->id }}">
                                 <input type="submit" value="トークへ移動">
                                 @csrf
                             </form>
-
                             {{-- グループの作成者のみが表示されるボタン --}}
-                            @if(Auth::id() == $group->administrator_id)
+                            @if (Auth::id() == $group->administrator_id)
                                 {{-- メンバーの編集 --}}
                                 <form action="/GroupList/edit" method="POST">
-                                    <input type="hidden" name="group_id" value="{{$group->id}}">
+                                    <input type="hidden" name="group_id" value="{{ $group->id }}">
                                     <input type="submit" value="グループの編集">
                                     @csrf
                                 </form>
                                 {{-- グループの削除 --}}
                                 <form action="/GroupList/delete" method="POST">
-                                    <input type="hidden" name="group_id" value="{{$group->id}}">
+                                    <input type="hidden" name="group_id" value="{{ $group->id }}">
                                     <input type="submit" value="グループの削除">
                                     @csrf
                                 </form>
                             @else
                                 {{-- メンバーを追加ボタン --}}
-                                <form action="/GroupList/add_member" method="POST">
-                                    <input type="hidden" name="id" value="{{$group->id}}">
-                                    <input type="submit" value="メンバーの追加">
-                                    @csrf
-                                </form>
+                                <div id='add_before_button'>
+                                    <button onclick="AddMember()">メンバーを追加</button>
+                                </div>
                             @endif
                             <span class="close">&times;</span>
                         </div>
@@ -141,18 +147,44 @@
 
         // 閉じるボタンがクリックされた時の処理
         for (var i = 0; i < closeBtns.length; i++) {
+            var divA = document.getElementById("add_before_members");
+            var divB = document.getElementById("add_before_button");
+            var divC = document.getElementById("add_after");
             closeBtns[i].onclick = function() {
                 var modal = this.parentNode.parentNode;
                 modal.style.display = "none"; // 対応するモーダルを非表示にする
+                divA.style.display = "block";
+                divB.style.display = "block";
+                divC.style.display = "none";
             };
         }
 
         // モーダルウィンドウの外側がクリックされた時の処理
         window.onclick = function(event) {
+            var divA = document.getElementById("add_before_members");
+            var divB = document.getElementById("add_before_button");
+            var divC = document.getElementById("add_after");
             for (var i = 0; i < modals.length; i++) {
                 if (event.target == modals[i]) {
                     modals[i].style.display = "none"; // 対応するモーダルを非表示にする
+                    divA.style.display = "block";
+                    divB.style.display = "block";
+                    divC.style.display = "none";
                 }
             }
         };
+
+        function AddMember() {
+            var divA = document.getElementById("add_before_members");
+            var divB = document.getElementById("add_before_button");
+            var divC = document.getElementById("add_after");
+
+            divA.style.display = "none";
+            divB.style.display = "none";
+            divC.style.display = "block";
+        }
     </script>
+    <style>
+        .hidden {
+            display: none;
+        }
