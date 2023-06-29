bcf0c012200fe2270ec9b9945a809e3d5ac10e75
ishiiyukina <ykn-ishi@usen-next.jp>
2023-06-29 15:19:00 +0900

    ボーダー関連のCSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             <div class="welcome_container" {{--class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10"--}}>
-                <img src="unnamed.png" alt="ロゴ">
+                <img src="Routeロゴ透過版.gif" alt="ロゴ">
                 <div class="welcome_button_container">
                     @if (Route::has('login'))
                         <div>
